a14de247068999d526a6ca485233ef71332c677f
Mika Reich <mikareich0@gmail.com>
2023-12-05 21:10:37 +0100

    Fix formatting issues in globals.css and utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+// ignore this line because more exports will be added
+// eslint-disable-next-line import/prefer-default-export
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
