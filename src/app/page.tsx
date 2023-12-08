1ada48304cf1826f9acb49c061e7142d9fc57aed
Käkäsh <marco.zillgen23@gmail.com>
2023-12-08 19:27:11 +0100

    Refactor mobile feed selector component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,11 @@ import { Button } from '@/components/ui/button'
 
 // make code testwise darkmode
 
-
-
 export default function Home() {
   return (
     <>
       <div className="lg:hidden">
-        <div className="flex h-screen flex-col items-center justify-center">
-          <FeedSelector />
-        </div>
+        <FeedSelector />
 
         <Navbar />
       </div>
--- src/components/mobile/feed-selector.tsx
+++ src/components/mobile/feed-selector.tsx
@@ -1,11 +1,15 @@
 function FeedSelector() {
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex h-1/2 w-1/2 items-center justify-center rounded-full bg-slate-200">
-        Explore
+    <div className="h-fit-content fixed left-0 top-0 flex w-full border-b border-slate-300 text-lg font-semibold">
+      <div className="flex w-1/2 items-center justify-center py-3">
+        <div className="flex items-center justify-center border-b-2 border-slate-500 px-6 pb-1  pt-2 ">
+          Explore
+        </div>
       </div>
-      <div className="flex h-1/2 w-1/2 items-center justify-center rounded-full bg-slate-200">
-        Explore
+      <div className="flex w-1/2 items-center justify-center py-3 ">
+        <div className="flex items-center justify-center px-6 pb-1 pt-2 ">
+          Followed
+        </div>
       </div>
     </div>
   )
