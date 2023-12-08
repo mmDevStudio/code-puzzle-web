5644b63f75049af06f64f0e4d4f51aa197faa1e2
Käkäsh <marco.zillgen23@gmail.com>
2023-12-08 17:11:12 +0100

    Add responsive design for mobile and desktop components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 // import {Post} from "@/components/Post";
 
+import Navbar from '@/components/mobile/navbar'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
+  // make responsive design if the screen is mobile or desktop
+  // if mobile, use mobile components
+  // if desktop, use desktop components
+
   return (
-    <div>
-      <Button>Click Me</Button>
-    </div>
+    <>
+      <div className="lg:hidden">
+        <Navbar />
+      </div>
+      <div className="hidden lg:block">
+        <div className="flex h-screen flex-col items-center justify-center">
+          <Button>Button</Button>
+        </div>
+      </div>
+    </>
   )
 }
