a71845a8d451faa1c27a6f59fbdbb031af3b37d8
Käkäsh <marco.zillgen23@gmail.com>
2023-12-08 19:41:08 +0100

    Refactor mobile navbar layout

--- src/components/mobile/navbar.tsx
+++ src/components/mobile/navbar.tsx
@@ -8,32 +8,42 @@ import {
 
 function Navbar() {
   return (
-    <div className="fixed bottom-0 left-0 flex h-16 w-full items-center justify-around border-t-2 bg-slate-100 dark:border-slate-800 dark:bg-slate-900">
-      <SearchIcon
-        height={32}
-        width={32}
-        className="text-slate-700 dark:text-slate-200"
-      />
-      <SendHorizontalIcon
-        height={32}
-        width={32}
-        className="text-slate-700 dark:text-slate-200"
-      />
-      <HomeIcon
-        height={32}
-        width={32}
-        className="text-slate-700 dark:text-slate-200"
-      />
-      <PuzzleIcon
-        height={32}
-        width={32}
-        className="text-slate-700 dark:text-slate-200"
-      />
-      <UserIcon
-        height={32}
-        width={32}
-        className="text-slate-700 dark:text-slate-200"
-      />
+    <div className="fixed bottom-0 left-0 flex h-16 w-full border-t-2 bg-slate-100 dark:border-slate-800 dark:bg-slate-900">
+      <div className="flex w-1/5 items-center justify-center">
+        <SearchIcon
+          height={32}
+          width={32}
+          className="text-slate-700 dark:text-slate-200"
+        />
+      </div>
+      <div className="flex w-1/5 items-center justify-center">
+        <SendHorizontalIcon
+          height={32}
+          width={32}
+          className="text-slate-700 dark:text-slate-200"
+        />
+      </div>
+      <div className="flex w-1/5 items-center justify-center">
+        <HomeIcon
+          height={32}
+          width={32}
+          className="text-slate-700 dark:text-slate-200 fill-current"
+        />
+      </div>
+      <div className="flex w-1/5 items-center justify-center">
+        <PuzzleIcon
+          height={32}
+          width={32}
+          className="text-slate-700 dark:text-slate-200"
+        />
+      </div>
+      <div className="flex w-1/5 items-center justify-center">
+        <UserIcon
+          height={32}
+          width={32}
+          className="text-slate-700 dark:text-slate-200"
+        />
+      </div>
     </div>
   )
 }
