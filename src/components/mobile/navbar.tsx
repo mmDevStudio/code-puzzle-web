79a3d02ec52b15720343ab74e94b193d809a73aa
Käkäsh <marco.zillgen23@gmail.com>
2023-12-08 22:08:51 +0100

    size of secondary items in nav bar 32px -> 24px

--- src/components/mobile/navbar.tsx
+++ src/components/mobile/navbar.tsx
@@ -11,15 +11,15 @@ function Navbar() {
     <div className="fixed bottom-0 left-0 flex h-16 w-full border-t-2 bg-slate-100 dark:border-slate-800 dark:bg-slate-900">
       <div className="flex w-1/5 items-center justify-center">
         <SearchIcon
-          height={32}
-          width={32}
+          height={24}
+          width={24}
           className="text-slate-700 dark:text-slate-200"
         />
       </div>
       <div className="flex w-1/5 items-center justify-center">
         <SendHorizontalIcon
-          height={32}
-          width={32}
+          height={24}
+          width={24}
           className="text-slate-700 dark:text-slate-200"
         />
       </div>
@@ -32,15 +32,15 @@ function Navbar() {
       </div>
       <div className="flex w-1/5 items-center justify-center">
         <PuzzleIcon
-          height={32}
-          width={32}
+          height={24}
+          width={24}
           className="text-slate-700 dark:text-slate-200"
         />
       </div>
       <div className="flex w-1/5 items-center justify-center">
         <UserIcon
-          height={32}
-          width={32}
+          height={24}
+          width={24}
           className="text-slate-700 dark:text-slate-200"
         />
       </div>
