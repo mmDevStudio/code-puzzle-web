a946cfa5839664ea339caadb893290b503263c9b
Käkäsh <marco.zillgen23@gmail.com>
2023-12-09 21:58:41 +0100

    Add vscode import and conditionally use it

--- src/components/routes/mobile/createPost.tsx
+++ src/components/routes/mobile/createPost.tsx
@@ -1,9 +1,18 @@
+'use client'
+
 import { ArrowLeft, Check } from 'lucide-react'
+import { useSearchParams } from 'next/navigation'
 
 import Headbar from '@/components/mobile/headbar'
 import Navbar from '@/components/mobile/navbar'
 
 function MobileCreatePost() {
+  const vscode: VSCodeImportedData = JSON.parse(
+    useSearchParams().get('vscode') || '{}'
+  )
+  if (vscode) {
+  }
+
   return (
     <>
       <Headbar
