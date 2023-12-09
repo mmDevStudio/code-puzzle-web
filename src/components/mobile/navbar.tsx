4d99ed5242e8bf0eade1e8b11790f94bd8cb8b7a
Käkäsh <marco.zillgen23@gmail.com>
2023-12-09 15:05:18 +0100

    Update navbar colors

--- src/components/mobile/navbar.tsx
+++ src/components/mobile/navbar.tsx
@@ -8,40 +8,40 @@ import {
 
 function Navbar() {
   return (
-    <div className="fixed bottom-0 left-0 flex h-16 w-full border-t-2 bg-slate-100 dark:border-slate-800 dark:bg-slate-900">
+    <div className="fixed bottom-0 left-0 flex h-16 w-full border-t-2 bg-slate-100 dark:border-stone-800 dark:bg-stone-900">
       <div className="flex w-1/5 items-center justify-center">
         <SearchIcon
           height={24}
           width={24}
-          className="text-slate-700 dark:text-slate-200"
+          className="text-slate-700 dark:text-stone-200"
         />
       </div>
       <div className="flex w-1/5 items-center justify-center">
         <SendHorizontalIcon
           height={24}
           width={24}
-          className="text-slate-700 dark:text-slate-200"
+          className="text-slate-700 dark:text-stone-200"
         />
       </div>
       <div className="flex w-1/5 items-center justify-center">
         <HomeIcon
           height={32}
           width={32}
-          className="fill-current text-slate-700 dark:text-slate-200"
+          className="fill-current text-slate-700 dark:text-stone-200"
         />
       </div>
       <div className="flex w-1/5 items-center justify-center">
         <PuzzleIcon
           height={24}
           width={24}
-          className="text-slate-700 dark:text-slate-200"
+          className="text-slate-700 dark:text-stone-200"
         />
       </div>
       <div className="flex w-1/5 items-center justify-center">
         <UserIcon
           height={24}
           width={24}
-          className="text-slate-700 dark:text-slate-200"
+          className="text-slate-700 dark:text-stone-200"
         />
       </div>
     </div>
