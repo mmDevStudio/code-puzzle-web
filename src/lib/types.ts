0a1a6e9c315824a79d1967bb6706fe116c2898f7
Käkäsh <marco.zillgen23@gmail.com>
2023-12-09 22:01:03 +0100

    Delete obsolete interfaces and add new types

---
+++ src/lib/types.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+type VSCodeImportedData = {
+  code: string
+  fileName: string
+  project: string
+  language: string
+}
+
+export type { VSCodeImportedData }
