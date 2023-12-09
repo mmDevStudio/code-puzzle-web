315b0d2cb8eb44221be9476d766d9fdcb053b6ca
Käkäsh <marco.zillgen23@gmail.com>
2023-12-09 21:58:31 +0100

    Add interfaces for VSCodeImportedData and sth else, bcz default export preferred xD

---
+++ src/lib/interfaces.ts
@@ -0,0 +1,13 @@
+interface VSCodeImportedData {
+  code: string
+  fileName: string
+  project: string
+  language: string
+}
+
+interface sth {
+  a: string
+  b: string
+}
+
+export { type VSCodeImportedData, type sth }
